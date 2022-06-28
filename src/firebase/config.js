ceba569694f9a3dc04033672d1f5985fc06eb82b
Bruno Peres <bruno_16515@hotmail.com>
2022-06-28 10:57:07 -0300

    First commit

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYxqlH0ugDLO5uWobgLeKw3jCBQzA-v54",
+  authDomain: "the-dojo-62004.firebaseapp.com",
+  projectId: "the-dojo-62004",
+  storageBucket: "the-dojo-62004.appspot.com",
+  messagingSenderId: "978547477564",
+  appId: "1:978547477564:web:a4415049acfbf006804739",
+};
+
+//Iniciar firebase
+firebase.initializeApp(firebaseConfig);
+
+//Iniciar Serviços
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+const timestamp = firebase.firestore.Timestamp();
+
+export { projectFirestore, projectAuth, timestamp };
