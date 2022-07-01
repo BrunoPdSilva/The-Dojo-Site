20d5e2a6d5314ecdea1a5fad9300e83dc2aaff51
Bruno Peres <bruno_16515@hotmail.com>
2022-07-01 10:01:55 -0300

    Estilização e comentários em tempo real

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Avatar } from "../../components/Avatar";
 import { timestamp } from "../../firebase/config";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useFirestore } from "../../hooks/useFirestore";
@@ -22,13 +23,32 @@ export function ProjectComments({ project }) {
     await updateDocument(project.id, {
       comments: [...project.comments, commentToAdd],
     });
-    
-    if (!response.error) {setNewComment('')}
+
+    if (!response.error) {
+      setNewComment("");
+    }
   };
 
   return (
     <div className="project-comments">
       <h4>Project comments</h4>
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map(comment => (
+            <li key={comment.id}>
+              <div className="comment-author">
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className="comment-date">
+                <p>DATE HERE</p>
+              </div>
+              <div className="comments-content">
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
 
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
